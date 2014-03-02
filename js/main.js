9eb3bc4bc1367dc6c82f13430b53fa070b20b49d
Derek Hurley <derek@idealist.org>
2014-03-01 23:37:39 -0800

    added dat.gui, now to expose things

--- js/main.js
+++ js/main.js
@@ -4,12 +4,18 @@ requirejs.config({
         jquery: '../bower_components/jquery/jquery.min',
         underscore: '../bower_components/underscore-amd/underscore-min',
 
+        dat: '../bower_components/dat.gui/dat.gui.min',
+
         three: '../bower_components/threejs/build/three.min',
         fly: './lib/FlyControls',
         orbital: './lib/OrbitControls'
     },
 
     shim: {
+        dat: {
+            exports: 'dat'
+        },
+
         three: {
             exports: 'THREE'
         },
@@ -27,8 +33,14 @@ requirejs.config({
 });
 
 requirejs([
-    'app'
-], function(App) {
+    'app',
+    'dat'
+], function(App, dat) {
     var app = window.App = new App();
     app.init();
+
+    var gui = new dat.GUI();
+    gui.add(app, 'spin');
+    gui.add(app, 'showHeadsUp');
+    gui.add(app, 'debug');
 });
