b22b46bbf8c6ac9cf120e9a7dedfdf0d8ae2247b
Derek Hurley <derek@idealist.org>
2014-03-04 01:39:19 -0800

    round artist.faces for closer guess, start thinking on face clusters

--- js/processor.js
+++ js/processor.js
@@ -3,26 +3,28 @@ define([
     'helpers',
     'three'
 ], function(_, h, THREE) {
-    var nextArtistCallCount = 0;
     var artist;
-
-    var stopSearching = function(totalArtists) {
-        return totalArtists === nextArtistCallCount;
-    };
+    var artistInfo;
+    var face;
+    var faces;
+    var facesToSet;
+    var nextArtistCallCount = 0;
+    var totalArtists;
 
     var nextArtist = function(artistIndex, data) {
-        if (artistIndex === data.length) {
+        if (artistIndex === totalArtists) {
             artistIndex = 0;
         }
         artist = data[artistIndex];
         if (artist.faces === 0) {
-            if (stopSearching(data.length)) {
+            if (nextArtistCallCount === totalArtists) {
                 return false;
             }
             nextArtistCallCount++;
             return nextArtist(artistIndex + 1, data);
         }
         artist.faces--;
+        artistIndex++;
         nextArtistCallCount = 0;
         return {
             artist: artist,
@@ -30,7 +32,12 @@ define([
         };
     };
 
-    var setFace = function(face, totalArtists, artistIndex, artist) {
+    var nextFace = function(artist, faceNum) {
+        face = faces[faceNum];
+        return face;
+    };
+
+    var setFace = function(face, artistIndex, artist) {
         face.color.setHex(h.spacedColor(totalArtists, artistIndex));
         face.color.multiplyScalar(artist.normCount);
         face.data = {
@@ -41,7 +48,6 @@ define([
 
     var updateFaces = function(data) {
         var artistIndex = 0,
-            faces = App.mesh.globe.geometry.faces,
             randos = h.randomBoundedArray(0, faces.length - 1);
 
         var totalPlays = _.reduce(data, function(memo, d) {
@@ -49,7 +55,7 @@ define([
         }, 0);
 
         _.map(data, function(artist) {
-            artist.faces = Math.floor(artist.playCount * faces.length / totalPlays);
+            artist.faces = Math.round(artist.playCount * faces.length / totalPlays);
             return artist;
         });
 
@@ -59,12 +65,15 @@ define([
                 return;
             }
             artistIndex = artistInfo.currentArtistIndex;
-            setFace(faces[randos[i]], data.length, artistIndex, artistInfo.artist);
+            faceToSet = nextFace(artist, randos[i]);
+            setFace(faceToSet, artistIndex, artistInfo.artist);
         }
     };
 
     return {
         process: function(data) {
+            faces = App.mesh.globe.geometry.faces;
+            totalArtists = data.length;
             updateFaces(data);
             App.mesh.update();
         }
