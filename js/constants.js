5db56fbf91b7a5f69edcb1afcc31466cd70cb527
Derek Hurley <dzhurley@gmail.com>
2015-03-31 16:57:54 -0700

    make constants really constant

--- js/constants.js
+++ js/constants.js
@@ -1,37 +1,36 @@
 // used on both app and worker side to create some sense of coherence
-var constants = {
-    globe: {
-        radius: 50,
-        widthAndHeight: 50
+
+const globe = Object.freeze({
+    radius: 50,
+    widthAndHeight: 50
+});
+
+const labels = Object.freeze({
+    'backgroundColor': '#272727',
+    'color': '#d7d7d7',
+    'fontface': 'Inconsolata',
+    'fontsize': '300'
+});
+
+const stars = Object.freeze({
+    number: 1000,
+
+    initialX: function() {
+        return Math.random() * 2 - 1;
+    },
+    initialY: function() {
+        return Math.random() * 2 - 1;
+    },
+    initialZ: function() {
+        return Math.random() * 2 - 1;
     },
 
-    labels: {
-        'backgroundColor': '#272727',
-        'color': '#d7d7d7',
-        'fontface': 'Inconsolata',
-        'fontsize': '300'
+    positionMultiplier: function() {
+        return Math.random() * 100 + 200;
     },
-
-    stars: {
-        number: 1000,
-
-        initialX: function() {
-            return Math.random() * 2 - 1;
-        },
-        initialY: function() {
-            return Math.random() * 2 - 1;
-        },
-        initialZ: function() {
-            return Math.random() * 2 - 1;
-        },
-
-        positionMultiplier: function() {
-            return Math.random() * 100 + 200;
-        },
-        scaleMultiplier: function() {
-            return Math.random() * 0.5;
-        }
+    scaleMultiplier: function() {
+        return Math.random() * 0.5;
     }
-};
+})
 
-module.exports = constants;
+module.exports = Object.freeze({ globe, labels, stars });
