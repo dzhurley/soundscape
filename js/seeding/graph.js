822d772893c4670fd2218b5b65f41d93a6484078
Derek Hurley <dzhurley@gmail.com>
2015-09-12 18:21:28 -1000

    merge THREE.Mesh into Node and THREE.Line into Edge to remove drawObject

--- js/seeding/graph.js
+++ js/seeding/graph.js
@@ -1,63 +1,92 @@
 'use strict';
 
-function Graph() {
-    this.nodeSet = {};
-    this.nodes = [];
-    this.edges = [];
+let THREE = require('three');
+
+class Graph {
+    constructor() {
+        this.nodeSet = {};
+        this.nodes = [];
+        this.edges = [];
+    }
+
+    addNode(node) {
+        if (typeof this.nodeSet[node.nid] === 'undefined') {
+            this.nodeSet[node.nid] = node;
+            this.nodes.push(node);
+            return node;
+        }
+        return false;
+    }
+
+    getNode(nodeId) {
+        return this.nodeSet[nodeId];
+    }
+
+    addEdge(source, target) {
+        if (source.addConnectedTo(target) === true) {
+            let edge = new Edge(source, target);
+            this.edges.push(edge);
+            return edge;
+        }
+        return false;
+    }
 }
 
-Graph.prototype.addNode = function(node) {
-    if (typeof this.nodeSet[node.nid] === 'undefined') {
-        this.nodeSet[node.nid] = node;
-        this.nodes.push(node);
-        return true;
+class Node extends THREE.Mesh {
+    constructor(nodeId) {
+        super(
+            new THREE.SphereGeometry(5, 5, 0),
+            new THREE.MeshBasicMaterial({color: Math.random() * 0xffffff})
+        );
+
+        let area = 10000;
+        this.position.x = Math.floor(Math.random() * (area + area + 1) - area);
+        this.position.y = Math.floor(Math.random() * (area + area + 1) - area);
+
+        this.nid = nodeId;
+        this.layout = { maxX: 90, minX: -90, maxY: 180, minY: -180 };
+
+        this.nodesTo = [];
+        this.nodesFrom = [];
     }
-    return false;
-};
 
-Graph.prototype.getNode = function(nodeId) {
-    return this.nodeSet[nodeId];
-};
-
-Graph.prototype.addEdge = function(source, target) {
-    if (source.addConnectedTo(target) === true) {
-        let edge = new Edge(source, target);
-        this.edges.push(edge);
-        return true;
-    }
-    return false;
-};
-
-function Node(nodeId) {
-    this.nid = nodeId;
-    this.nodesTo = [];
-    this.nodesFrom = [];
-    this.position = {};
-    this.data = {};
-}
-
-Node.prototype.addConnectedTo = function(node) {
-    if (this.connectedTo(node) === false) {
-        this.nodesTo.push(node);
-        return true;
-    }
-    return false;
-};
-
-Node.prototype.connectedTo = function(node) {
-    for (let i = 0; i < this.nodesTo.length; i++) {
-        let connectedNode = this.nodesTo[i];
-        if (connectedNode.nid === node.nid) {
+    addConnectedTo(node) {
+        if (this.connectedTo(node) === false) {
+            this.nodesTo.push(node);
             return true;
         }
+        return false;
     }
-    return false;
-};
 
-function Edge(source, target) {
-    this.source = source;
-    this.target = target;
-    this.data = {};
+    connectedTo(node) {
+        for (let i = 0; i < this.nodesTo.length; i++) {
+            let connectedNode = this.nodesTo[i];
+            if (connectedNode.nid === node.nid) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class Edge extends THREE.Line {
+    constructor(source, target) {
+        super(
+            new THREE.Geometry(),
+            new THREE.LineBasicMaterial({
+                color: 0xCCCCCC, opacity: 1, linewidth: 0.5
+            }),
+            THREE.LinePieces
+        );
+
+        this.geometry.vertices.push(source.position);
+        this.geometry.vertices.push(target.position);
+        this.scale.x = this.scale.y = this.scale.z = 1;
+        this.originalScale = 1;
+
+        this.source = source;
+        this.target = target;
+    }
 }
 
 module.exports = { Graph, Node, Edge };
--- js/seeding/sphere.js
+++ js/seeding/sphere.js
@@ -16,7 +16,6 @@ var SphereGraph = function() {
     var maxY = radius;
     var minY = -radius;
 
-    var geometry = new THREE.SphereGeometry( 5, 5, 0 );
     var graph = new Graph();
 
     var numNodes = 50;
@@ -25,9 +24,9 @@ var SphereGraph = function() {
 
     function positionUpdated(node) {
         if (node.nid === 0) {
-            node.data.drawObject.position.x = 0;
-            node.data.drawObject.position.y = 0;
-            node.data.drawObject.position.z = 0;
+            node.position.x = 0;
+            node.position.y = 0;
+            node.position.z = 0;
             return;
         }
         maxX = Math.max(maxX, node.position.x);
@@ -35,65 +34,37 @@ var SphereGraph = function() {
         maxY = Math.max(maxY, node.position.y);
         minY = Math.min(minY, node.position.y);
 
-        let lat, lng;
-        if (node.position.x < 0) {
-            lat = -90 / minX * node.position.x;
-        } else {
-            lat = 90 / maxX * node.position.x;
-        }
-        if (node.position.y < 0) {
-            lng = -180 / minY * node.position.y;
-        } else {
-            lng = 180 / maxY * node.position.y;
-        }
+        let lat = node.position.x < 0 ?
+            -90 / minX * node.position.x :
+            90 / maxX * node.position.x;
+        let lng = node.position.y < 0 ?
+            -180 / minY * node.position.y :
+            180 / maxY * node.position.y;
 
         let area = 55;
         let phi = (90 - lat) * Math.PI / 180;
         let theta = (180 - lng) * Math.PI / 180;
-        node.data.drawObject.position.x = area * Math.sin(phi) * Math.cos(theta);
-        node.data.drawObject.position.y = area * Math.cos(phi);
-        node.data.drawObject.position.z = area * Math.sin(phi) * Math.sin(theta);
-    }
-
-    function drawNode(node) {
-        var drawObject = new THREE.Mesh(
-            geometry,
-            new THREE.MeshBasicMaterial({ color: Math.random() * 0xffffff })
-        );
-
-        var area = 10000;
-        drawObject.position.x = Math.floor(Math.random() * (area + area + 1) - area);
-        drawObject.position.y = Math.floor(Math.random() * (area + area + 1) - area);
-
-        node.position.x = Math.floor(Math.random() * (area + area + 1) - area);
-        node.position.y = Math.floor(Math.random() * (area + area + 1) - area);
-
-        drawObject.nid = node.nid;
-        node.data.drawObject = drawObject;
-        node.layout = {};
-        node.layout.maxX = 90;
-        node.layout.minX = -90;
-        node.layout.maxY = 180;
-        node.layout.minY = -180;
-
-        // node.position = drawObject.position;
-        scene.add( node.data.drawObject );
+        node.position.x = area * Math.sin(phi) * Math.cos(theta);
+        node.position.y = area * Math.cos(phi);
+        node.position.z = area * Math.sin(phi) * Math.sin(theta);
     }
 
     function createGraph() {
         var steps = 1;
         var startNode = new Node(0);
         graph.addNode(startNode);
-        drawNode(startNode);
+        scene.add(startNode);
 
         while (steps < numNodes) {
             let edges = randomFromTo(1, 10);
             for (let i = 1; i <= edges; i++) {
                 let targetNode = new Node(i * steps);
                 if (graph.addNode(targetNode)) {
-                    drawNode(targetNode);
-                    if (graph.addEdge(startNode, targetNode)) {
-                        drawEdge(startNode, targetNode);
+                    scene.add(targetNode);
+                    let edge = graph.addEdge(startNode, targetNode);
+                    if (edge) {
+                        window.seedGeometries.push(edge.geometry);
+                        scene.add(edge);
                     }
                 }
             }
@@ -101,24 +72,6 @@ var SphereGraph = function() {
         }
 
         return new ForceDirected(graph, { positionUpdated });
-    }
-
-    function drawEdge(source, target) {
-        var material = new THREE.LineBasicMaterial({
-            color: 0xCCCCCC, opacity: 1, linewidth: 0.5
-        });
-        var tmpGeo = new THREE.Geometry();
-
-        tmpGeo.vertices.push(source.data.drawObject.position);
-        tmpGeo.vertices.push(target.data.drawObject.position);
-
-        let line = new THREE.Line(tmpGeo, material, THREE.LinePieces);
-        line.scale.x = line.scale.y = line.scale.z = 1;
-        line.originalScale = 1;
-
-        window.seedGeometries.push(tmpGeo);
-
-        scene.add(line);
     }
 
     function randomFromTo(from, to) {
