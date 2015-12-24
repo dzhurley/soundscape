8738760440dd58bdf50e98577ebec83c5b265b6e
Derek Hurley <dzhurley@gmail.com>
2015-12-24 14:36:41 -0700

    declass sources

--- js/app.js
+++ js/app.js
@@ -4,13 +4,14 @@ const Worker = require('worker!./worker');
 
 const { addWorker } = require('./dispatch');
 const Threes = require('./three/main');
-const Sourcer = require('./sources/main');
+const { registerSources } = require('./sources/main');
 const { attachWebGLement } = require('./dom');
 
 addWorker(new Worker());
 
 Threes.setScene();
 
-Sourcer.addSources(['lastfm']);
+// TODO: constants
+registerSources(['lastfm']);
 
 attachWebGLement(Threes.renderer.domElement);
--- js/sources/main.js
+++ js/sources/main.js
@@ -19,76 +19,58 @@
 const { packUrlParams, randomArray } = require('../helpers');
 const { emit, emitOnWorker, on } = require('../dispatch');
 
-class Sourcer {
-    constructor() {
-        this.sources = {};
-        on('submitting', this.checkSource.bind(this));
-    }
+// TODO: find better state solution (localStorage?)
+let registered = {};
 
-    addSources(sources) {
-        for (let src of sources) {
-            this.sources[src] = require(`./${src}`);
+const registerSources = sources => sources.map(src => registered[src] = require(`./${src}`));
+
+const getArtists = (source, username) => {
+    if (Object.keys(localStorage).indexOf(username) > -1) {
+        let artists = JSON.parse(localStorage[username]);
+
+        if (artists) {
+            emitOnWorker('plot.seed', JSON.stringify(artists));
+            emit('submitted');
+            return;
         }
     }
 
-    sourceUrl(params = {}) {
-        params = Object.assign({}, this.activeSource.defaultParams, params);
-        return packUrlParams(this.activeSource.baseUrl, params);
+    let request = new XMLHttpRequest();
+    let params = Object.assign({}, source.defaultParams, source.paramsForUser(username));
+    request.open('GET', packUrlParams(source.baseUrl, params), true);
+
+    request.onload = () => {
+        if (request.status >= 200 && request.status < 400) {
+            let artists = source.parseData(JSON.parse(request.responseText));
+            let stringified = JSON.stringify(randomArray(artists));
+            localStorage[username] = stringified;
+            emitOnWorker('plot.seed', stringified);
+            emit('submitted');
+        }
+    };
+
+    request.send();
+};
+
+const checkSource = evt => {
+    evt.preventDefault();
+    let source = evt.target.querySelector('#source').value;
+    let username = evt.target.querySelector('#username').value;
+
+    // TODO: be nicer
+    if (!Object.keys(registered).indexOf(source) < 0) {
+        console.error(`Invalid source: ${source}`);
+        return false;
     }
-
-    checkSource(evt) {
-        evt.preventDefault();
-        let source = evt.target.querySelector('#source').value;
-        let username = evt.target.querySelector('#username').value;
-
-        // TODO be nicer
-        if (!Object.keys(this.sources).indexOf(source) < 0) {
-            console.error(`Invalid source: ${source}`);
-            return false;
-        }
-        if (username.length === 0) {
-            console.error('No username given');
-            return false;
-        }
-
-        this.startSource(source, username);
+    if (username.length === 0) {
+        console.error('No username given');
         return false;
     }
 
-    startSource(source, username) {
-        this.activeSource = this.sources[source];
-        this.getArtistsForUser(username);
+    getArtists(registered[source], username);
+    return false;
+};
 
-        emit('submitted');
-        this.artists = null;
-    }
+on('submitting', checkSource);
 
-    getArtistsForUser(username) {
-        if (Object.keys(localStorage).indexOf(username) > -1) {
-            this.artists = JSON.parse(localStorage[username]);
-
-            if (this.artists) {
-                emitOnWorker('plot.seed', JSON.stringify(this.artists));
-                return;
-            }
-        }
-
-        let url = this.sourceUrl(this.activeSource.paramsForUser(username));
-        let request = new XMLHttpRequest();
-        request.open('GET', url, true);
-
-        request.onload = () => {
-            if (request.status >= 200 && request.status < 400) {
-                let data = JSON.parse(request.responseText);
-                this.artists = this.activeSource.parseData(data);
-                let stringified = JSON.stringify(randomArray(this.artists));
-                emitOnWorker('plot.seed', stringified);
-                localStorage[username] = stringified;
-            }
-        };
-
-        request.send();
-    }
-}
-
-module.exports = new Sourcer;
+module.exports = { registerSources };
