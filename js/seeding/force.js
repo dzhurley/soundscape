601ae00e8ea62e19731e9a5127310b353ae49088
Derek Hurley <dzhurley@gmail.com>
2016-11-22 19:43:24 -0800

    start painting a voronoi (from convex hull) with seed colors

--- js/seeding/force.js
+++ js/seeding/force.js
@@ -29,7 +29,7 @@ const applyDiff = (v, u) => {
 const iterateForce = () => {
     if (iterations >= maxIterations || temp <= epsilon) {
         iterable = false;
-        emit('seed', Array.from(nodes).map(n => n.position));
+        emit('seed', Array.from(nodes));
         return;
     }
 
--- js/seeding/paint.js
+++ js/seeding/paint.js
@@ -2,20 +2,70 @@ const THREE = require('three');
 THREE.ConvexGeometry = require('exports?THREE.ConvexGeometry!three/examples/js/geometries/ConvexGeometry');
 
 const { on } = require('dispatch');
+const { faceCentroid } = require('helpers');
 const scene = require('three/scene');
 
-const applyConvex = vertices => {
+const convexFor = vertices => {
     const hull = new THREE.Mesh(
         new THREE.ConvexGeometry(vertices),
-        new THREE.MeshBasicMaterial({ color: 0xFF0000, wireframe: true })
+        new THREE.MeshBasicMaterial({
+            shading: THREE.FlatShading,
+            side: THREE.DoubleSide,
+            vertexColors: THREE.FaceColors
+        })
     );
-    hull.scale.multiplyScalar(1.05);
-    scene.add(hull);
+    hull.geometry.mergeVertices();
+    return hull;
 };
 
-const paint = vertices => {
-    applyConvex(vertices);
-    // TODO find voronoi, use globe face normals to paint
+const normal = (mesh, face) => faceCentroid(mesh, face).normalize();
+
+const findClosestSeed = (mesh, candidates, target) => {
+    let closest, newDistance, lastDistance;
+    for (let i = 0; i < candidates.length; i++) {
+        newDistance = normal(mesh, target).distanceTo(candidates[i]);
+        if (!closest) {
+            closest = candidates[i];
+            lastDistance = newDistance;
+        } else if (newDistance < lastDistance) {
+            closest = candidates[i];
+            lastDistance = newDistance;
+        }
+    }
+    return closest;
+};
+
+const voronoiFromHull = hull => {
+    const voronoi = convexFor(hull.geometry.faces.map(face => face.normal), 0x0000FF);
+    voronoi.scale.multiplyScalar(315);
+
+    const caster = new THREE.Raycaster();
+    hull.geometry.vertices.map(vertex => {
+        caster.set(scene.position, vertex.clone().normalize());
+        caster.intersectObject(voronoi)[0].face.color.set(vertex.color);
+    });
+
+    voronoi.geometry.faces.map(face => {
+        const closest = findClosestSeed(voronoi, hull.geometry.vertices, face);
+        face.color.set(closest.color);
+    });
+
+    return voronoi;
+};
+
+const paint = seeds => {
+    const vertices = seeds.map(seed => {
+        seed.position.name = seed.name;
+        seed.position.color = seed.material.color;
+        return seed.position;
+    });
+
+    const hull = convexFor(vertices);
+
+    const voronoi = voronoiFromHull(hull);
+    scene.add(voronoi);
+
+    // TODO: paint
 };
 
 const bindPainter = () => on('seed', paint);
